59427ea8289367eae4d765dddcff2b1f0b0e6734
Arthur Blot <arthur.blot78@gmail.com>
2015-06-03 18:34:41 +0200

    Start proving create.v

--- src/create.v
+++ src/create.v
@@ -7,11 +7,34 @@ Require Import specs props.
 Definition create {n} (b: bool): BITS n
   := if b then dropmsb (subB (shlBn (n := n.+1) #1 n) #1) else #0.
 
+Lemma toNat_shlBn:
+  forall n (p: BITS n) k, k < n ->
+    toNat (shlBn p k) = (toNat p) * 2^k.
+Proof.
+  admit.
+Admitted.
+
 Lemma makeOnes:
   forall n,
     ones n = dropmsb (subB (shlBn (n := n.+1) #1 n) #1).
 Proof.
-  admit.
+  move=> n.
+  have: toNat (ones n) = toNat (dropmsb (subB (shlBn (n := n.+1) #1 n) #1)).
+    rewrite toNat_ones.
+    rewrite toNat_dropmsb.
+    rewrite subB1.
+    rewrite toNat_decB.
+    rewrite toNat_shlBn.
+    have H: ~(shlBn #1 n == #0).
+      by admit.
+    elim H': (shlBn #1 n == #0).
+      admit. (* whatever *)
+    rewrite toNat_fromNat //.
+    have ->: div.modn (div.modn 1 (2 ^ n.+1) * 2 ^ n).-1 (2 ^ n) = (2 ^ n).-1.
+      by admit.
+    trivial.
+    trivial.
+    apply toNat_inj.
 Admitted.
 
 Lemma create_repr:
