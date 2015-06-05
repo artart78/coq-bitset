be90a0fa17f0451cf5f3cfb18987283bc94cd65a
Arthur Blot <arthur.blot78@gmail.com>
2015-06-05 14:37:01 +0200

    Add a non-proven pop implementation

--- src/pop.v
+++ src/pop.v
@@ -1,5 +1,5 @@
 From Ssreflect
-     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun.
+     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun div.
 From Bits
      Require Import bits.
 Require Import specs props off_r.
@@ -32,7 +32,7 @@ Proof.
       apply bs_neqz.
       rewrite bs_eqz //.
 Qed.
-  
+
 (*
 Definition pop {n}(bs: BITS n): nat
   := let fix count n x :=
@@ -47,3 +47,45 @@ Proof.
   admit.
 Admitted.
 *)
+(*
+Definition pop_mask_seq {n}(k: nat): seq bool
+  := let fix aux n (k: 'I_k) b :=
+       match n with
+       | 0 => [::]
+       | n'.+1 =>
+         let b' := (if k == 0 then ~~ b else b) in
+         cons b' (aux n' k.+1 b')
+       end
+     in aux n 0 false.
+
+  match xs with
+    | [::] => [::]
+    | cons false xs => cons false (off_r_seq xs)
+    | cons true xs => cons false xs
+  end.
+*)
+
+Definition pop_mask_seq {n}(k: nat): seq bool
+  := let fix aux n :=
+       match n with
+       | 0 => [::]
+       | n'.+1 => ((n' %% (2 * k)) >= k) :: aux n'
+       end
+     in aux n.
+
+Lemma pop_maskP {n}(k: nat): size (pop_mask_seq (n := n) k) == n.
+Proof.
+  elim: n=> [|n IHn] //=.
+Qed.
+
+Canonical pop_mask {n} (k: nat): BITS n
+  := Tuple (pop_maskP k).
+
+Definition pop {n}(bs: BITS (2^n)): nat
+  := let fix count n k x :=
+       match n with
+       | 0 => x
+       | n'.+1 => let x' := addB (andB x (pop_mask k)) (andB (shrBn x k) (pop_mask k)) in
+                  count n' k.*2 x'
+       end
+     in toNat(count n 1 bs).
