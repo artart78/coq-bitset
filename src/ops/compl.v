c8211831fbc10125b48755d8ddda56689011d28e
Arthur Blot <arthur.blot78@gmail.com>
2015-06-11 11:09:43 +0200

    Spec for compl & symdiff

--- src/ops/compl.v
+++ src/ops/compl.v
@@ -1,22 +1,17 @@
 From Ssreflect
-     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun.
+     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun finset.
 From Bits
      Require Import bits.
-Require Import props.getbit.
+Require Import props.getbit spec.
 
 Definition compl {n} (bs: BITS n): BITS n
   := invB bs.
 
-(* TODO: This lemma is not very pleasing: I would expect the
-implementation [compl] to be on the left (without [getBit]) and a
-high-level spec of the set to be on the right. However, I don't quite
-see how to make that happen. It shall become clearer when we write the
-spec for the library. *)
-
 Lemma compl_repr:
-  forall n (bs: BITS n) k, k < n ->
-    getBit (compl bs) k = ~~ (getBit bs k).
+  forall n (bs: BITS n) E, repr bs E ->
+    repr (compl bs) (~: E).
 Proof.
-  move=> n bs k le_k.
-  by rewrite getBit_liftUnOp.
+  move=> n bs E HE.
+  rewrite /repr -setP /eq_mem=> x.
+  by rewrite in_setC HE !in_set getBit_liftUnOp.
 Qed.
--- src/ops/symdiff.v
+++ src/ops/symdiff.v
@@ -1,16 +1,23 @@
 From Ssreflect
-     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun.
+     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun finset.
 From Bits
      Require Import bits.
-Require Import props.getbit.
+Require Import props.getbit spec.
 
 Definition symdiff {n} (bs: BITS n) (bs': BITS n): BITS n
   := xorB bs bs'.
 
 Lemma symdiff_repr:
-  forall n (bs: BITS n) (bs': BITS n) k, k < n ->
-    getBit (symdiff bs bs') k = xorb (getBit bs k) (getBit bs' k).
+  forall n (bs: BITS n) (bs': BITS n) E E', repr bs E -> repr bs' E' ->
+    repr (symdiff bs bs') ((E :\: E') :|: (E' :\: E)).
 Proof.
-  move=> n bs bs' k le_k.
-  by rewrite getBit_liftBinOp.
+  move=> n bs bs' E E' HE HE'.
+  rewrite /repr -setP /eq_mem=> x.
+  rewrite in_setU !in_setD.
+  rewrite /symdiff /xorB HE HE' !in_set getBit_liftBinOp=> //.
+  case: (getBit bs x)=> //.
+  + (* getBit bs x = true *)
+    by rewrite andbT orbF Bool.xorb_true_l.
+  + (* getBit bs x = false *)
+    by rewrite andbF orbC orbF andbC andbT Bool.xorb_false_l.
 Qed.
