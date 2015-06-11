5280a304d0a7742139c7e4a612dfb6a83ae2cd77
Arthur Blot <arthur.blot78@gmail.com>
2015-06-11 17:15:37 +0200

    Write the specification & start proving keep_min

--- src/ops/keep_min.v
+++ src/ops/keep_min.v
@@ -1,34 +1,44 @@
 From Ssreflect
-     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun.
+     Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun finset.
 From Bits
      Require Import bits tuple.
-Require Import props.bineqs.
+Require Import props.bineqs spec.
 
 Definition keep_min {n} (bs: BITS n): BITS n
   := andB bs (negB bs).
 
 Lemma keep_min_repr:
-  forall n (bs: BITS n),
-    andB bs (negB bs) = setBit #0 (index true bs) true.
+  forall n (bs: BITS n) E x, repr bs E -> x \in E ->
+    repr (andB bs (negB bs)) [set [arg min_(k < x in E) k]].
 Proof.
-  elim=> [bs|n IHn /tupleP[b bs]].
+  elim=> [bs|n IHn /tupleP[b bs]] E x HE Hx.
   + (* bs ~ [tuple] *)
-    by rewrite tuple0.
+    exfalso.
+    have H: E = set0.
+      admit.
+    rewrite H in Hx.
+    by rewrite in_set0 in Hx=> //.
   + (* bs ~ [tuple of b :: bs] *)
-    case: b.
+    case: b HE=> HE.
     - (* b ~ true *)
       rewrite /negB /incB /invB /andB /=.
       rewrite liftUnOpCons tuple.beheadCons.
       rewrite liftBinOpCons andbT.
       rewrite -/andB -/invB.
       rewrite andB_invB.
-      rewrite tuple.beheadCons /=.
-      by rewrite fromNat0 //.
+      rewrite /repr.
+      rewrite -setP /eq_mem=> i.
+      rewrite !in_set.
+      case: arg_minP=> //.
+      move=> i' Hi' Hj.
+      have ->: getBit (consB true (zero n)) i = (i == ord0).
+        by admit.
+      by admit. (* ord0 \in E -> i' <= ord0 -> i' = ord0 *)
     - (* b ~ false *)
       rewrite /negB /incB /invB /andB /=.
       rewrite liftUnOpCons tuple.beheadCons.
       rewrite liftBinOpCons andbF.
-      rewrite -/andB -/invB -/incB -/negB.
-      rewrite IHn.
-      by rewrite tuple.theadCons tuple.beheadCons //=.
-Qed.
+      rewrite -/andB -/invB -/incB.
+      by admit. (* Harder: we have to show a result over sets of 'I_n.+1 from a result over sets of 'I_n.
+      Maybe using arg_minP & the fact that '<=' is the same for nat and ordinals is enough? *)
+Admitted.
