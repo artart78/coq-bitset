68cd65c4d0ee0f16ff73badf9bbdd327e2198f03
Arthur Blot <arthur.blot78@gmail.com>
2015-06-16 15:48:21 +0200

    Prove most of index_repr

--- src/ops/min.v
+++ src/ops/min.v
@@ -61,7 +61,90 @@ Lemma index_repr:
   forall n (bs: BITS n) x E, repr bs E -> x \in E ->
     index true bs = [arg min_(k < x in E) k].
 Proof.
-  admit.
+  elim=> [bs|n IHn /tupleP[b bs]] x E HE Hx.
+  + (* n ~ 0 *)
+    by move: x Hx=> [x le_x].
+  + (* n ~ n.+1 *)
+    case: b HE=> HE.
+    - (* b ~ true *)
+      rewrite /=.
+      case: arg_minP=> // i Hi Hj.
+      have H: i <= 0.
+        rewrite (Hj ord0) //.
+        rewrite HE in_set //=.
+      move/eqP: H=>H.
+      rewrite subn0 in H.
+      apply esym.
+      by rewrite H.
+    - (* b ~ false *)
+      rewrite /=.
+      set E' := [ set x : 'I_n | inord(x.+1) \in E ].
+      have HpredK: (n.-1).+1 = n by admit. (* We can't have n = 0, otherwise repr [false] E and x \in E, which is not possible! *)
+      set x' := cast_ord HpredK (inord (n' := n.-1) x.-1).
+      have Hx': x' \in E'.
+        rewrite in_set.
+        rewrite /=.
+        rewrite inordK.
+        have ->: x.-1.+1 = x by admit.
+        have ->: inord (n' := n) x = x.
+          apply ord_inj.
+          rewrite inordK //.
+        apply Hx.
+        admit. (* x.-1 < n.-1.+1 *)
+      have HE': repr bs E' by apply (repr_rec n bs E false).
+      case: arg_minP=> // i Hi Hj.
+      rewrite (IHn bs x' E')=> //.
+      case: arg_minP=> // i' Hi' Hj'.
+      have H1: i <= inord (n' := n) i'.+1.
+        rewrite (Hj (inord i'.+1)) //.
+        rewrite HE.
+        rewrite in_set.
+        have ->: getBit [tuple of false :: bs] (inord (n' := n) i'.+1) = getBit bs i'.
+          rewrite inordK //.
+          rewrite -[i'.+1]addn1 -[n.+1]addn1.
+          rewrite ltn_add2r.
+          rewrite ltn_ord //.
+        rewrite HE' in Hi'.
+        rewrite in_set in Hi'.
+        apply Hi'.
+      have H2: i' <= cast_ord HpredK (inord (n' := n.-1) i.-1).
+        rewrite (Hj' (cast_ord HpredK (inord (n' := n.-1) i.-1))) //.
+        rewrite HE'.
+        rewrite in_set.
+        have ->: getBit bs (cast_ord HpredK (inord i.-1)) = getBit [tuple of false :: bs] i.
+          rewrite /=.
+          have {2}->: nat_of_ord i = i.-1.+1.
+            by admit.
+          have ->: getBit [tuple of false :: bs] i.-1.+1 = getBit bs i.-1 by compute.
+          rewrite inordK //.
+          admit. (* i.-1 < n.-1.+1 *)
+        rewrite HE in Hi.
+        rewrite in_set in Hi.
+        apply Hi.
+      have H': i'.+1 == i.
+        rewrite eqn_leq.
+        have ->: i <= i'.+1.
+          rewrite inordK in H1.
+          apply H1.
+          rewrite -[i'.+1]addn1 -[n.+1]addn1.
+          rewrite ltn_add2r //.
+        have ->: i'.+1 <= i.
+          rewrite /= in H2.
+          rewrite inordK in H2.
+          have ->: nat_of_ord i = i.-1.+1.
+            by admit. (* See above. *)
+          rewrite -[i'.+1]addn1 -[i.-1.+1]addn1.
+          rewrite leq_add2r.
+          apply H2.
+          rewrite !prednK.
+          rewrite -(leq_add2r 1).
+          rewrite !addn1.
+          rewrite ltn_ord //.
+          admit. (* 0 < n *)
+          admit. (* 0 < i *)
+        rewrite //=.
+      move/eqP: H' ->.
+      rewrite //.
 Admitted.
 
 Lemma ntz_repr:
