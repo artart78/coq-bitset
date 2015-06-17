6061e8b44a89086633ae7532b161823bd9f0a255
Arthur Blot <arthur.blot78@gmail.com>
2015-06-17 15:39:34 +0200

    Clean keep_min.v

--- src/ops/keep_min.v
+++ src/ops/keep_min.v
@@ -7,27 +7,6 @@ Require Import props.bineqs props.getbit spec min.
 Definition keep_min {n} (bs: BITS n): BITS n
   := andB bs (negB bs).
 
-Lemma singleton_repr:
-  forall n (k: 'I_n), repr (setBit #0 k true) [set k].
-Proof.
-  move=> n k.
-  rewrite /repr -setP /eq_mem=> x.
-  rewrite !in_set.
-  case H: (x == k).
-  + (* x == k *)
-    move/eqP: H ->.
-    by rewrite setBitThenGetSame.
-  + (* x <> k *)
-    rewrite setBitThenGetDistinct=> //.
-    rewrite getBit_zero //.
-    apply not_eq_sym.
-    move=> H'.
-    move/eqP: H=>H.
-    apply H.
-    apply ord_inj.
-    by apply H'.
-Qed.
-
 Lemma keep_min_repr:
   forall n (bs: BITS n) E x, repr bs E -> x \in E ->
     repr (andB bs (negB bs)) [set [arg min_(k < x in E) k]].
@@ -40,21 +19,14 @@ Proof.
   + (* bs ~ [tuple of b :: bs] *)
     case: b.
     - (* b ~ true *)
-      rewrite /negB /incB /invB /andB /=.
-      rewrite liftUnOpCons tuple.beheadCons.
-      rewrite liftBinOpCons andbT.
-      rewrite -/andB -/invB.
-      rewrite andB_invB.
-      rewrite tuple.beheadCons /=.
-      by rewrite fromNat0 //.
+      by rewrite /negB /incB /invB /andB /=
+                 liftUnOpCons !tuple.beheadCons liftBinOpCons andbT
+                 -/andB -/invB andB_invB fromNat0.
     - (* b ~ false *)
-      rewrite /negB /incB /invB /andB /=.
-      rewrite liftUnOpCons tuple.beheadCons.
-      rewrite liftBinOpCons andbF.
-      rewrite -/andB -/invB -/incB -/negB.
-      rewrite IHn.
-      by rewrite tuple.theadCons tuple.beheadCons //=.
+      by rewrite /negB /incB /invB /andB /=
+                 liftUnOpCons tuple.theadCons !tuple.beheadCons liftBinOpCons andbF
+                 -/andB -/invB -/incB -/negB IHn.
   move=> E x HE Hx.
   rewrite (index_repr n bs x E)=> //.
-  apply singleton_repr.
+  by apply singleton_repr.
 Qed.
--- src/spec.v
+++ src/spec.v
@@ -2,8 +2,7 @@ From Ssreflect
      Require Import ssreflect ssrbool eqtype ssrnat seq tuple fintype ssrfun finset.
 From Bits
      Require Import bits.
-
-Require Import Omega.
+Require Import props.getbit.
 
 Definition repr {n}(bs: BITS n) E :=
   E = [ set x : 'I_n | getBit bs x ].
@@ -18,4 +17,24 @@ Proof.
   rewrite /getBit -nth_behead //=.
   have ltn_i: i < n by apply ltn_ord.
   by auto with arith.
+Qed.
+
+Lemma singleton_repr:
+  forall n (k: 'I_n), repr (setBit #0 k true) [set k].
+Proof.
+  move=> n k.
+  rewrite /repr -setP /eq_mem=> x.
+  rewrite !in_set.
+  case x_eq_k: (x == k).
+  + (* x == k *)
+    move/eqP: x_eq_k ->.
+    by rewrite setBitThenGetSame.
+  + (* x <> k *)
+    rewrite setBitThenGetDistinct=> //.
+    rewrite getBit_zero //.
+    apply not_eq_sym.
+    move=> x_is_k.
+    move/eqP: x_eq_k=>x_eq_k.
+    apply x_eq_k.
+    by apply ord_inj.
 Qed.
