b847ea8ed5a0f4c8c659da002c2e3477295f214e
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2015-06-28 22:29:57 +0200

    Add a todo note to tozp

--- src/props/tozp.v
+++ src/props/tozp.v
@@ -3,23 +3,21 @@ From Ssreflect
 From Bits
      Require Import bits.
 
+(* TODO: this shall eventually be moved to coq-bits:operations/properties.v *)
+
 Lemma toZp_shlBn:
   forall n (p: BITS n) k, k < n ->
     toZp (shlBn p k) = (toZp p * (2 ^ k)%:R)%R.
 Proof.
-  move=> n p k le_k.
-  elim: k le_k=> [|k IHk] le_k.
+  move=> n p.
+  elim=> [|k IHk] le_k.
   + (* Case: k ~ 0 *)
-    rewrite /shlBn /=.
     by rewrite expn0 GRing.mulr_natr //.
   + (* Case: k ~ k + 1 *)
     rewrite /shlBn iterS -[iter k shlB p]/(shlBn _ _).
     rewrite toZp_shlB.
     rewrite IHk; last by auto with arith.
-    rewrite expnS.
-    rewrite mulnC.
-    rewrite !GRing.mulr_natr.
-    rewrite GRing.mulrnA //.
+    by rewrite expnS mulnC !GRing.mulr_natr GRing.mulrnA.
 Qed.
 
 Lemma toNat_shlBn:
@@ -29,12 +27,12 @@ Proof.
   elim=> [le_k|k IHk le_k].
   + (* k ~ 0 *)
     rewrite toNat_fromNat modn_small=> //.
-    have {1}->: 1 = 2 ^ 0 by rewrite //.
-    by rewrite ltn_exp2l //.
+    have {1}->: 1 = 2 ^ 0 by trivial.
+    by rewrite ltn_exp2l.
   + (* k ~ k.+1 *)
     rewrite toNat_shlB IHk.
     rewrite -muln2.
-    have {2}->: 2 = 2 ^ 1 by rewrite //.
+    have {2}->: 2 = 2 ^ 1 by trivial.
     rewrite -expnD addn1 modn_small // ltn_exp2l //.
     auto with arith.
 Qed.
