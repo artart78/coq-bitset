9cc3762f6071468739df0f2c9eb8971ad7ddaaad
Arthur Blot <arthur.blot78@gmail.com>
2016-06-11 00:38:55 +0900

    Enum_parts: continue proof

--- examples/enum_parts.v
+++ examples/enum_parts.v
@@ -5,24 +5,71 @@ From Bits
 
 Require Import repr_op.
 
-Definition enumNext (x: Int32) :=
-  let smallest := land x (neg x) in
-  let ripple := add smallest x in
+Definition enumNext (x: Int32) := (* 111001100 *)
+  let smallest := keep_min x in(* 000000100 *)
+  let ripple := add smallest x in  (* 111010000 *)
   let ones := lsr (lxor x ripple) (add (toInt 2) (ntz x)) in
   lor ripple ones.
 
+Definition set_next (S: {set 'I_wordsize}) := [arg min_(x < ord0 | ((x \notin S) && [exists y, (y \in S) && (y < x)])) x].
+
+Definition set_min (S: {set 'I_wordsize}) e := [arg min_(x < e in S) x].
+
 Definition enumNext_set (S: {set 'I_wordsize}) e (H: e \in S) :=
-  let s_min := [arg min_(x < e in S) x] in
-  let s_next := [arg min_(x < ord0 | ((x \notin S) && [exists y, (y \in S) && (y < x)])) x] in
+  let s_min := set_min S e in
+  let s_next := set_next S in
   [set s_next] :|: [set x in S | x > s_next] :|: [set x in 'I_wordsize | x <= (s_next - s_min - 2)].
 
+Lemma srn_repr:
+  forall i E s, machine_repr i E ->
+    machine_repr (lsr i (toInt s)) [set i : 'I_wordsize | i < wordsize - s & @inord wordsize.-1 (i + s) \in E].
+Proof.
+Admitted.
+
 Lemma enumNext_correct (x: Int32) (S: {set 'I_wordsize}) e (H: e \in S) :
+  set_next S <> ord0 ->
   machine_repr x S -> machine_repr (enumNext x) (enumNext_set S e H).
 Proof.
-move=> Hx.
-apply union_repr.
-admit.
-admit.
+move=> Hnext Hx.
+have repr_ripple: machine_repr (add (keep_min x) x)
+     (set_next S |: [set x0 in S | set_next S < x0]).
+  by admit.
+apply union_repr=> //.
+have ->: add (toInt 2) (ntz x) = toInt (2 + set_min S e).
+  by admit.
+have ->: [set x0 in 'I_wordsize | x0 <= set_next S - set_min S e - 2] =
+[set i : 'I_wordsize | i < wordsize - (2 + set_min S e) & @inord wordsize.-1 (i + (2 + set_min S e)) \in ([set set_next S] :|: [set x in S | x < set_next S])].
+  apply/setP=> i; rewrite !inE.
+  rewrite andbC andbT.
+  rewrite [in _ || (_ && _)]andb_idl.
+  rewrite -leq_eqVlt.
+  admit.
+  admit.
+apply srn_repr.
+set E' := (set_next S |: [set x0 in S | set_next S < x0]).
+have ->: (set_next S |: [set x0 in S | x0 < set_next S]) = (S :\: E') :|: (E' :\: S).
+  apply/setP=> i; rewrite !inE.
+  rewrite negb_or negb_and.
+  rewrite andb_orr.
+  rewrite andb_orl.
+  rewrite -andbA.
+  rewrite andNb andbF.
+  symmetry.
+  rewrite -orbA orbC orbF.
+  rewrite andb_orr.
+  rewrite orbA.
+  rewrite andbA.
+  rewrite andNb orbF.
+  rewrite -leqNgt.
+  rewrite -ltn_neqAle.
+  rewrite orbC.
+  rewrite andb_idl.
+  by rewrite andbC.
+  move: Hnext.
+  rewrite /set_next /arg_min.
+  case: pickP=> [y //= Ha _ /eqP->|//].
+  by move/andP: Ha => [/andP [-> _] _].
+apply symdiff_repr=> //.
 Admitted.
 
 Cd "examples/enum_parts".
